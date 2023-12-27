68771fec4ae9bfed04b7fa0b0fc4b989ba9c4498
Carlos Henry Jr <carlo.henrique37@gmail.com>
2023-12-27 20:46:21 -0300

    Adição de comentários sobre o Scanner

--- PraticandoJavaVScode/Ex001.java
+++ PraticandoJavaVScode/Ex001.java
@@ -6,6 +6,7 @@ public class Ex001 {
 
     public static void main(String[] args) throws Exception{
         
+        //Scanner para captura de dados aberto
         Scanner entrada = new Scanner(System.in);
 
         System.out.print("Qual seu nome?: ");
@@ -13,6 +14,7 @@ public class Ex001 {
 
         System.out.println("É um prazer conhecer você " + nome);
 
+        //Escaner fechado
         entrada.close();
     }
 }
